772335b7f65781933c8e4465fa3a11d6186dd256
WillChilds-Klein <willck93@gmail.com>
2023-07-13 20:19:55 +0000

    Install s2n and awslc as deps, use s2nd instead of nginx

--- emulation-exp/code/install-prereqs-ubuntu.sh
+++ emulation-exp/code/install-prereqs-ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+set -o pipefail
 
 sudo apt update
 sudo apt install -y git \
@@ -10,35 +11,49 @@ sudo apt install -y git \
                ninja-build \
                libssl-dev \
                libpcre3-dev \
+               golang-go \
                wget
 
 NGINX_VERSION=1.17.5
 CMAKE_VERSION=3.18
 CMAKE_BUILD=3
 
+rm -rf tmp
 mkdir -p tmp
 cd tmp
 ROOT=$(pwd)
+INSATLL_DIR=${ROOT}/install
+rm -rf ${INSATLL_DIR}
+mkdir -p ${INSATLL_DIR}
 
 # Fetch all the files we need
 wget https://cmake.org/files/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.${CMAKE_BUILD}-Linux-x86_64.sh
+wget nginx.org/download/nginx-${NGINX_VERSION}.tar.gz && tar -zxvf nginx-${NGINX_VERSION}.tar.gz
 git clone --single-branch --branch pq-tls-experiment https://github.com/xvzcf/liboqs
 git clone --single-branch --branch pq-tls-experiment https://github.com/xvzcf/openssl
-wget nginx.org/download/nginx-${NGINX_VERSION}.tar.gz && tar -zxvf nginx-${NGINX_VERSION}.tar.gz
+git clone --single-branch --branch main https://github.com/WillChilds-Klein/s2n-tls
+git clone --single-branch --branch main https://github.com/WillChilds-Klein/aws-lc
 
 # Install the latest CMake
 mkdir cmake
 sh cmake-${CMAKE_VERSION}.${CMAKE_BUILD}-Linux-x86_64.sh --skip-license --prefix=${ROOT}/cmake
+CMAKE=${ROOT}/cmake/bin/cmake
 
 # build liboqs
-cd liboqs
-mkdir build && cd build
-${ROOT}/cmake/bin/cmake -GNinja -DCMAKE_INSTALL_PREFIX=${ROOT}/openssl/oqs ..
+pushd liboqs
+rm -rf build
+mkdir build
+pushd build
+${CMAKE} \
+    -GNinja \
+    -DCMAKE_INSTALL_PREFIX=${ROOT}/openssl/oqs \
+    ..
 ninja && ninja install
+popd    # build
+popd    # liboqs
 
 # build nginx (which builds OQS-OpenSSL)
-cd ${ROOT}
-cd nginx-${NGINX_VERSION}
+pushd nginx-${NGINX_VERSION}
 ./configure --prefix=${ROOT}/nginx \
                 --with-debug \
                 --with-http_ssl_module --with-openssl=${ROOT}/openssl \
@@ -47,4 +62,38 @@ cd nginx-${NGINX_VERSION}
                 --with-ld-opt="-L ${ROOT}/openssl/oqs/lib";
 sed -i 's/libcrypto.a/libcrypto.a -loqs/g' objs/Makefile;
 sed -i 's/EVP_MD_CTX_create/EVP_MD_CTX_new/g; s/EVP_MD_CTX_destroy/EVP_MD_CTX_free/g' src/event/ngx_event_openssl.c;
-make && make install;
+make
+make install
+popd
+
+# build AWS-LC
+pushd aws-lc
+rm -rf build
+mkdir -p build
+pushd build
+${CMAKE} \
+    -DFIPS=1 \
+    -DCMAKE_PREFIX_PATH=${INSATLL_DIR} \
+    -DCMAKE_INSTALL_PREFIX=${INSATLL_DIR} \
+    -DCMAKE_VERBOSE_MAKEFILE=1 \
+    -DENABLE_DILITHIUM=ON \
+    ..
+make -j $(nproc) 2>&1
+make install -j $(nproc)
+popd    # build
+popd    # aws-lc
+
+# build s2n
+pushd s2n-tls
+rm -rf build
+mkdir -p build
+pushd build
+${CMAKE} \
+    -DCMAKE_PREFIX_PATH=${INSATLL_DIR} \
+    -DCMAKE_INSTALL_PREFIX=${INSATLL_DIR} \
+    -DCMAKE_VERBOSE_MAKEFILE=1 \
+    ..
+make -j $(nproc) 2>&1
+make install -j $(nproc)
+popd    # build
+popd    # s2n-tls
--- emulation-exp/code/kex/setup.sh
+++ emulation-exp/code/kex/setup.sh
@@ -6,8 +6,8 @@ ROOT="$(dirname $(pwd))"
 OPENSSL=${ROOT}/tmp/openssl/apps/openssl
 OPENSSL_CNF=${ROOT}/tmp/openssl/apps/openssl.cnf
 
-NGINX_APP=${ROOT}/tmp/nginx/sbin/nginx
 NGINX_CONF_DIR=${ROOT}/tmp/nginx/conf
+S2ND=${ROOT}/tmp/s2n-tls/build/bin/s2nd
 
 ##########################
 # Build s_timer
@@ -34,8 +34,24 @@ ${OPENSSL} req -new -newkey ec:prime256v1.pem -keyout ${NGINX_CONF_DIR}/server.k
 # generate server cert
 ${OPENSSL} x509 -req -in ${NGINX_CONF_DIR}/server.csr -out ${NGINX_CONF_DIR}/server.crt -CA ${NGINX_CONF_DIR}/CA.crt -CAkey ${NGINX_CONF_DIR}/CA.key -CAcreateserial -days 365
 
+function cleanup() {
+    ##########################
+    # Remove files
+    ##########################
+    rm -f prime256v1.pem \
+          s_timer.o
+
+    ##########################
+    # Remove network namespaces
+    ##########################
+    sudo ip netns del cli_ns
+    sudo ip netns del srv_ns
+}
+trap cleanup INT TERM
+
 ##########################
 # Start nginx
 ##########################
-cp nginx.conf ${NGINX_CONF_DIR}/nginx.conf
-sudo ip netns exec srv_ns ${NGINX_APP}
+sudo ip netns exec srv_ns ${S2ND} 10.0.0.1 4433 || /bin/true
+
+cleanup
