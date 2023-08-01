c627ea3058ef53396eafb8fa5fcebefe815a9f08
WillChilds-Klein <willck93@gmail.com>
2023-08-01 15:06:21 +0000

    Static build against s2n and AWS-LC works for s_timer

--- emulation-exp/code/kex/s_timer.c
+++ emulation-exp/code/kex/s_timer.c
@@ -6,14 +6,17 @@
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
  */
+#include <arpa/inet.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <time.h>
 
 #include <s2n.h>
-
-#include <time.h>
 
 #define NS_IN_MS 1000000.0
 #define MS_IN_S 1000
@@ -34,55 +37,58 @@ int do_tls_handshake(struct s2n_connection *conn)
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(4433); 
 
-    if(inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
+    if(inet_pton(AF_INET, host, &serv_addr.sin_addr) < 1) {
         fprintf(stderr, "Error: inet_pton failed\n");
         return -1;
     } 
 
     if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-       fprintf(stderr, "Error: connect failed\n");
-       return -1;
+       fprintf(stderr, "Error: connect failed with %s\n", strerror(errno));
+       return errno;
     } 
 
     struct linger no_linger = {.l_onoff = 1, .l_linger = 0};
     if (setsockopt(sockfd, SOL_SOCKET, SO_LINGER, (char*)&no_linger, sizeof(no_linger)) < 0) {
-        fprintf(stderr, "Error: setting LINGER=0 sockopt failed");
+        fprintf(stderr, "Error: setting LINGER=0 sockopt failed with %s\n", strerror(errno));
+        return errno;
+    }
+
+    if (s2n_connection_set_fd(conn, sockfd) != S2N_SUCCESS) {
+        fprintf(stderr, "Error: failed to set fd on connection. %s: %s\n",
+                s2n_strerror(s2n_errno, NULL), s2n_strerror_debug(s2n_errno, NULL));
         return -1;
     }
 
-    s2n_blocked_status blocked = S2N_BLOCKED;
-    if (s2n_negotiate(conn, &blocked) != S2N_SUCCESS) {
-        fprintf(stderr, "Error: %s. %s\n", s2n_strerror(s2n_errno, NULL), s2n_strerror_debug(s2n_errno, NULL));
+    s2n_blocked_status unused;
+    if (s2n_negotiate(conn, &unused) != S2N_SUCCESS) {
+        fprintf(stderr, "Error: failed to negotiate. %s: %s\n",
+                s2n_strerror(s2n_errno, NULL), s2n_strerror_debug(s2n_errno, NULL));
         return -1;
     }
 
-    return 0;
+    return sockfd;
 }
 
 int main(int argc, char* argv[])
 {
     int ret = -1;
-    SSL_CTX* ssl_ctx = 0;
     if(argc != 3)
     {
-        fprintf(stderr, "Wrong number of arguments.\n");
+        fprintf(stderr, "Usage: %s <kex_alg> <measurement_count>\n", argv[0]);
         goto end;
     }
-    const char* kex_alg = argv[1];
+    /*const char* kex_alg = argv[1];*/
     const size_t measurements_to_make = strtol(argv[2], 0, 10);
     size_t measurements = 0;
-
-    const char* ciphersuites = "TLS_AES_256_GCM_SHA384";
-    const SSL_METHOD* ssl_meth = TLS_client_method();
-    SSL* ssl = NULL;
 
     struct timespec start, finish;
     double* handshake_times_ms = malloc(measurements_to_make * sizeof(*handshake_times_ms));
 
+    /*
     ssl_ctx = SSL_CTX_new(ssl_meth);
     if (!ssl_ctx)
     {
-        goto ossl_error;
+        goto s2n_err;
     }
 
     SSL_CTX_set_mode(ssl_ctx, SSL_MODE_AUTO_RETRY);
@@ -91,13 +97,13 @@ int main(int argc, char* argv[])
     ret = SSL_CTX_set_min_proto_version(ssl_ctx, TLS1_3_VERSION);
     if (ret != 1)
     {
-        goto ossl_error;
+        goto s2n_err;
     }
 
     ret = SSL_CTX_set_max_proto_version(ssl_ctx, TLS1_3_VERSION);
     if (ret != 1)
     {
-        goto ossl_error;
+        goto s2n_err;
     }
 
     SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_COMPRESSION);
@@ -105,48 +111,68 @@ int main(int argc, char* argv[])
     ret = SSL_CTX_set_ciphersuites(ssl_ctx, ciphersuites);
     if (ret != 1)
     {
-        goto ossl_error;
+        goto s2n_err;
     }
     ret = SSL_CTX_set1_groups_list(ssl_ctx, kex_alg);
     if (ret != 1)
     {
-        goto ossl_error;
+        goto s2n_err;
     }
 
     ret = SSL_CTX_load_verify_locations(ssl_ctx, "../tmp/nginx/conf/CA.crt", 0);
     if(ret != 1)
     {
-        goto ossl_error;
+        goto s2n_err;
     }
     SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER, NULL);
+    */
 
-    struct *s2n_config = s2n_config_new();
+    struct s2n_connection *conn = s2n_connection_new(S2N_CLIENT);
+    if (conn == NULL) {
+        fprintf(stderr, "Error: failed to allocate new connection\n");
+        goto s2n_err;
+    }
+
+    struct s2n_config *config = s2n_config_new();
+    if (config == NULL) {
+        fprintf(stderr, "Error: failed to allocate new config\n");
+        goto s2n_err;
+    }
+    // TODO: configure !!
+    /*const char* ciphersuites = "TLS_AES_256_GCM_SHA384";*/
+    if (s2n_connection_set_config(conn, config)) {
+        fprintf(stderr, "Error: failed to set config on connection\n");
+        goto s2n_err;
+    }
+
     // TODO: point this at the trust store?
-    s2n_config_set_cipher_preferences("PQ-TLS-1-3-2023-06-01");
 
     while(measurements < measurements_to_make)
     {
         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-        ssl = do_tls_handshake(ssl_ctx);
+        // TODO: pull socket creation up here?
+        int sockfd = do_tls_handshake(conn);
         clock_gettime(CLOCK_MONOTONIC_RAW, &finish);
-        if (!ssl)
-        {
+        if (sockfd < 0) {
             /* Retry since at high packet loss rates,
              * the connect() syscall fails sometimes.
              * Non-retryable errors are caught by manual
              * inspection of logs, which has sufficed
              * for our purposes */
-            continue;
+            // TODO: need to scope down the retry condition to specific socket errors
+            //continue;
+            goto err;
         }
 
-        SSL_set_shutdown(ssl, SSL_SENT_SHUTDOWN | SSL_RECEIVED_SHUTDOWN);
-        ret = BIO_closesocket(SSL_get_fd(ssl));
-        if(ret == -1)
-        {
-            goto ossl_error;
+        s2n_blocked_status unused;
+        if(s2n_shutdown(conn, &unused) != S2N_SUCCESS || s2n_connection_wipe(conn) != S2N_SUCCESS) {
+            goto s2n_err;
         }
 
-        SSL_free(ssl);
+        if (shutdown(sockfd, SHUT_RDWR)) {
+            fprintf(stderr, "Error: socket shutdown failed with error %s\n", strerror(errno));
+            goto err;
+        }
 
         handshake_times_ms[measurements] = ((finish.tv_sec - start.tv_sec) * MS_IN_S) + ((finish.tv_nsec - start.tv_nsec) / NS_IN_MS);
         measurements++;
@@ -161,10 +187,12 @@ int main(int argc, char* argv[])
     ret = 0;
     goto end;
 
-ossl_error:
-    fprintf(stderr, "Unrecoverable OpenSSL error.\n");
-    ERR_print_errors_fp(stderr);
+s2n_err:
+    fprintf(stderr, "Error: Unrecoverable s2n error. %s: %s\n",
+            s2n_strerror(s2n_errno, NULL), s2n_strerror_debug(s2n_errno, NULL));
+err:
+    ret = 1;
 end:
-    SSL_CTX_free(ssl_ctx);
+    s2n_connection_free(conn);
     return ret;
 }
